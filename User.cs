95b952521316f6540712cc5bdea100d630ee3b83
kartikkher <kartikkher777@gmail.com>
2019-10-10 00:28:58 +0100

    user password encryption and username storage to (.txt file for time being) functionality added

---
+++ User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spital
+{
+    class User
+    {
+        // encryption key 
+        private string encryptionPassword = "7689iknh7564fbg6ghjgbt6";
+        private string storage = "users.txt";
+        private string password;
+
+        public string Username { get; set; }
+
+        public string Passowrd
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                // encreypt password before stroting into the file or database 
+                password = Encryption.EncryptText(value, encryptionPassword);
+            }
+        }
+
+        public bool ValidateCredential()
+        {
+            bool valid = false;
+            try
+            {
+                //open user.txt storage file
+                StreamReader reader = new StreamReader(storage);
+
+                //while theree aree lines to read 
+                while (!reader.EndOfStream)
+                {
+                    //**replace this with database connection
+                    //seperator used to break apart each file line
+                    char[] separators = { ',' };
+                    //break the line - the result is an array containing each part of the lines that was seperated by the above seperator
+                    string[] line = reader.ReadLine().Split(separators);
+                    //**
+
+                    string usernameToCheck = line[0];
+                    string passwordToCheck = line[1];
+
+                    if (Username == usernameToCheck && password == passwordToCheck)
+                    {
+                        valid = true;
+                        break;
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.FileName + "Please contact System Administrator");
+            }
+
+            return valid;
+
+        }
+    }
+}

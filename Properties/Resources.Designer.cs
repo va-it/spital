6ed3a7d1ceee7bbca980e16773d17ecc3d2b7400
kartikkher <kartikkher777@gmail.com>
2019-10-12 20:02:43 +0100

    Registration page UI added

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace spital.Properties
-{
-
-
+namespace spital.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,94 @@ namespace spital.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("spital.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bg_eaht {
+            get {
+                object obj = ResourceManager.GetObject("bg-eaht", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_Password {
+            get {
+                object obj = ResourceManager.GetObject("icon_Password", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icon_User {
+            get {
+                object obj = ResourceManager.GetObject("icon_User", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap leftDock {
+            get {
+                object obj = ResourceManager.GetObject("leftDock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo_EAHT {
+            get {
+                object obj = ResourceManager.GetObject("Logo_EAHT", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
---
+++ Registration.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace spital
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_username = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.label_password = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label_confirmPassword = new System.Windows.Forms.Label();
+            this.textBox_ConfirmPassword = new System.Windows.Forms.TextBox();
+            this.button_Register = new System.Windows.Forms.Button();
+            this.panel_line_username = new System.Windows.Forms.Panel();
+            this.panel_line_password = new System.Windows.Forms.Panel();
+            this.panel_line_confirmPassword = new System.Windows.Forms.Panel();
+            this.label_registration = new System.Windows.Forms.Label();
+            this.label_copyRight = new System.Windows.Forms.Label();
+            this.pictureBox_password = new System.Windows.Forms.PictureBox();
+            this.pictureBox_user = new System.Windows.Forms.PictureBox();
+            this.pictureBox_leftDock = new System.Windows.Forms.PictureBox();
+            this.pictureBox_confirmPassword = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_password)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_user)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_leftDock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_confirmPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_username.Location = new System.Drawing.Point(424, 159);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(62, 15);
+            this.label_username.TabIndex = 3;
+            this.label_username.Text = "Username";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_username.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_username.Location = new System.Drawing.Point(431, 177);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(175, 16);
+            this.textBox_username.TabIndex = 4;
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.label_password.Location = new System.Drawing.Point(424, 207);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(61, 15);
+            this.label_password.TabIndex = 5;
+            this.label_password.Text = "Password";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_password.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_password.Location = new System.Drawing.Point(431, 224);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(175, 16);
+            this.textBox_password.TabIndex = 6;
+            this.textBox_password.UseSystemPasswordChar = true;
+            // 
+            // label_confirmPassword
+            // 
+            this.label_confirmPassword.AutoSize = true;
+            this.label_confirmPassword.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.label_confirmPassword.Location = new System.Drawing.Point(424, 255);
+            this.label_confirmPassword.Name = "label_confirmPassword";
+            this.label_confirmPassword.Size = new System.Drawing.Size(107, 15);
+            this.label_confirmPassword.TabIndex = 7;
+            this.label_confirmPassword.Text = "Confirm Password";
+            this.label_confirmPassword.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // textBox_ConfirmPassword
+            // 
+            this.textBox_ConfirmPassword.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox_ConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_ConfirmPassword.Font = new System.Drawing.Font("Calibri", 9.75F);
+            this.textBox_ConfirmPassword.Location = new System.Drawing.Point(431, 273);
+            this.textBox_ConfirmPassword.Name = "textBox_ConfirmPassword";
+            this.textBox_ConfirmPassword.Size = new System.Drawing.Size(175, 16);
+            this.textBox_ConfirmPassword.TabIndex = 8;
+            this.textBox_ConfirmPassword.UseSystemPasswordChar = true;
+            this.textBox_ConfirmPassword.WordWrap = false;
+            // 
+            // button_Register
+            // 
+            this.button_Register.AutoSize = true;
+            this.button_Register.BackColor = System.Drawing.Color.SkyBlue;
+            this.button_Register.FlatAppearance.BorderSize = 0;
+            this.button_Register.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Register.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Register.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_Register.Location = new System.Drawing.Point(469, 318);
+            this.button_Register.Name = "button_Register";
+            this.button_Register.Size = new System.Drawing.Size(75, 29);
+            this.button_Register.TabIndex = 9;
+            this.button_Register.Text = "Register";
+            this.button_Register.UseVisualStyleBackColor = false;
+            // 
+            // panel_line_username
+            // 
+            this.panel_line_username.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel_line_username.Location = new System.Drawing.Point(426, 194);
+            this.panel_line_username.Name = "panel_line_username";
+            this.panel_line_username.Size = new System.Drawing.Size(180, 1);
+            this.panel_line_username.TabIndex = 10;
+            // 
+            // panel_line_password
+            // 
+            this.panel_line_password.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel_line_password.Location = new System.Drawing.Point(427, 242);
+            this.panel_line_password.Name = "panel_line_password";
+            this.panel_line_password.Size = new System.Drawing.Size(180, 1);
+            this.panel_line_password.TabIndex = 11;
+            // 
+            // panel_line_confirmPassword
+            // 
+            this.panel_line_confirmPassword.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel_line_confirmPassword.Location = new System.Drawing.Point(426, 290);
+            this.panel_line_confirmPassword.Name = "panel_line_confirmPassword";
+            this.panel_line_confirmPassword.Size = new System.Drawing.Size(180, 1);
+            this.panel_line_confirmPassword.TabIndex = 11;
+            // 
+            // label_registration
+            // 
+            this.label_registration.AutoSize = true;
+            this.label_registration.BackColor = System.Drawing.Color.Transparent;
+            this.label_registration.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_registration.Location = new System.Drawing.Point(383, 102);
+            this.label_registration.Name = "label_registration";
+            this.label_registration.Size = new System.Drawing.Size(114, 26);
+            this.label_registration.TabIndex = 12;
+            this.label_registration.Text = "Registration";
+            // 
+            // label_copyRight
+            // 
+            this.label_copyRight.AutoSize = true;
+            this.label_copyRight.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_copyRight.Location = new System.Drawing.Point(414, 426);
+            this.label_copyRight.Name = "label_copyRight";
+            this.label_copyRight.Size = new System.Drawing.Size(193, 15);
+            this.label_copyRight.TabIndex = 0;
+            this.label_copyRight.Text = "© 2019 East Anglia Hospital Trust";
+            // 
+            // pictureBox_password
+            // 
+            this.pictureBox_password.Image = global::spital.Properties.Resources.icon_Password;
+            this.pictureBox_password.Location = new System.Drawing.Point(388, 209);
+            this.pictureBox_password.Name = "pictureBox_password";
+            this.pictureBox_password.Size = new System.Drawing.Size(30, 34);
+            this.pictureBox_password.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_password.TabIndex = 15;
+            this.pictureBox_password.TabStop = false;
+            // 
+            // pictureBox_user
+            // 
+            this.pictureBox_user.Image = global::spital.Properties.Resources.icon_User;
+            this.pictureBox_user.Location = new System.Drawing.Point(388, 161);
+            this.pictureBox_user.Name = "pictureBox_user";
+            this.pictureBox_user.Size = new System.Drawing.Size(30, 32);
+            this.pictureBox_user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_user.TabIndex = 14;
+            this.pictureBox_user.TabStop = false;
+            // 
+            // pictureBox_leftDock
+            // 
+            this.pictureBox_leftDock.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox_leftDock.Image = global::spital.Properties.Resources.leftDock;
+            this.pictureBox_leftDock.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_leftDock.Name = "pictureBox_leftDock";
+            this.pictureBox_leftDock.Size = new System.Drawing.Size(335, 461);
+            this.pictureBox_leftDock.TabIndex = 13;
+            this.pictureBox_leftDock.TabStop = false;
+            // 
+            // pictureBox_confirmPassword
+            // 
+            this.pictureBox_confirmPassword.Image = global::spital.Properties.Resources.icon_Password;
+            this.pictureBox_confirmPassword.Location = new System.Drawing.Point(388, 257);
+            this.pictureBox_confirmPassword.Name = "pictureBox_confirmPassword";
+            this.pictureBox_confirmPassword.Size = new System.Drawing.Size(30, 34);
+            this.pictureBox_confirmPassword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_confirmPassword.TabIndex = 16;
+            this.pictureBox_confirmPassword.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Location = new System.Drawing.Point(388, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(218, 1);
+            this.panel1.TabIndex = 17;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox_confirmPassword);
+            this.Controls.Add(this.pictureBox_password);
+            this.Controls.Add(this.pictureBox_user);
+            this.Controls.Add(this.pictureBox_leftDock);
+            this.Controls.Add(this.label_copyRight);
+            this.Controls.Add(this.label_registration);
+            this.Controls.Add(this.panel_line_confirmPassword);
+            this.Controls.Add(this.panel_line_password);
+            this.Controls.Add(this.panel_line_username);
+            this.Controls.Add(this.button_Register);
+            this.Controls.Add(this.textBox_ConfirmPassword);
+            this.Controls.Add(this.label_confirmPassword);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.label_username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Registration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_password)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_user)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_leftDock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_confirmPassword)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label_confirmPassword;
+        private System.Windows.Forms.TextBox textBox_ConfirmPassword;
+        private System.Windows.Forms.Button button_Register;
+        private System.Windows.Forms.Panel panel_line_username;
+        private System.Windows.Forms.Panel panel_line_password;
+        private System.Windows.Forms.Panel panel_line_confirmPassword;
+        private System.Windows.Forms.Label label_registration;
+        private System.Windows.Forms.Label label_copyRight;
+        private System.Windows.Forms.PictureBox pictureBox_leftDock;
+        private System.Windows.Forms.PictureBox pictureBox_user;
+        private System.Windows.Forms.PictureBox pictureBox_password;
+        private System.Windows.Forms.PictureBox pictureBox_confirmPassword;
+        private System.Windows.Forms.Panel panel1;
+    }
+}

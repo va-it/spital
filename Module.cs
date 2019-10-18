565e3d966ff3967d91784afa953564d55a97e8dd
Vincenzo Abete <abetevincenzo@gmail.com>
2019-10-18 19:35:28 +0100

    WIP: started implementing module and monitor classes

---
+++ Module.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spital
+{
+    class Module
+    {
+        private int Id { get; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public Module()
+        {
+            this.Id = 1;
+            this.Name = "Heart Rate";
+            this.Icon = "heart_rate.png";
+            this.Min = 80;
+            this.Max = 180;
+        }
+    }
+}
---
+++ Monitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spital
+{
+    class Monitor
+    {
+        private int Id { get; }
+        public List<Module> Modules { get; set; }
+
+        public Monitor(Module[] modules)
+        {
+            this.Id = 1;
+
+            foreach (Module module in modules)
+            {
+                this.Modules.Add(module);
+            }
+            
+        }
+    }
+}
